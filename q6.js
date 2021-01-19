6d3c9f77ab9d5a21d1804ac2e794c8b62fe6fee4
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-01-19 16:21:01 +0200

    q6 done

--- q6.js
+++ q6.js
@@ -1,7 +1,25 @@
+
+
+
 function run() {
     const input = document.getElementById("input").value;
     const output = document.getElementById("output");
     let out = "";
+    let num=parseInt(input);
+    let i=10;
+    let arr=[];
+    while(Math.floor(num)>0){
+        let temp=Math.floor(num%10);
+        arr.unshift(temp);
+        num/=10;
+    }
+    for(let i=0;i<arr.length-1;i++){
+        if(arr[i]%2===0&arr[i+1]%2===0)
+            arr.splice(i+1,0,"-");
+    }
+    out=arr.toString();
+    while(out.includes(","))
+        out=out.replace(",","");
     
     output.innerText = out;
 }
