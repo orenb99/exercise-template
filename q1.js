9783488a8bf7cc10b85ca2c2ed256b8cc3d43bad
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-01-19 14:49:58 +0200

    q1 done

--- q1.js
+++ q1.js
@@ -2,10 +2,20 @@ function run() {
     const input = document.getElementById("input").value;
     const output = document.getElementById("output");
     let out = "";
-    // if(input.charAt(0)==="["&&input.charAt(input.length-1)==="]")
-    //     out=true;
-    // else
-    //     out=false;
-    // output.innerText = out;
+    let bol=true;
+    try {
+          JSON.parse(input);
+    } catch {
+          bol=false;
+        }
+    if(bol===true){
+    if(Array.isArray(JSON.parse(input)))
+        out=true;
+    else
+        out=false
+    }
+    else
+        out=false;
+    output.innerText = out;
 }
     
