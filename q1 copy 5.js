b03362d39b93dd121312b406515b13e8d3560a82
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-01-19 14:16:40 +0200

    question1 done

--- q1 copy 5.js
+++ q1 copy 5.js
@@ -2,10 +2,7 @@ function run() {
     const input = document.getElementById("input").value;
     const output = document.getElementById("output");
     let out = "";
-    if(input.charAt(0)==="["&&input.charAt(input.length-1)==="]")
-        out=true;
-    else
-        out=false;
+    
     output.innerText = out;
 }
     
--- q3.js
+++ q3.js
@@ -2,10 +2,8 @@ function run() {
     const input = document.getElementById("input").value;
     const output = document.getElementById("output");
     let out = "";
-    if(input.charAt(0)==="["&&input.charAt(input.length-1)==="]")
-        out=true;
-    else
-        out=false;
+    
+    
     output.innerText = out;
 }
     
