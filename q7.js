5a24ff40f750f99083fa14b8279992db40d4283c
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-01-19 18:31:35 +0200

    q7 done
    ceated an extra function that helps to get the max value of an array

--- q7.js
+++ q7.js
@@ -1,9 +1,33 @@
+function maxArray(arr){
+  let max=0;
+  let highest=true;
+  for(let i=0;i<arr.length;i++){
+    for(let j=0;j<arr.length;j++){
+      if(j!==i){
+        if(arr[i]>=arr[j]){
+          highest=true;
+        }
+        if(arr[i]<arr[j]){
+          highest=false;
+          break;
+        }
+      }
+    }
+    if(highest===true){
+      max=arr[i];
+    }
+    highest=true;
+  }
+  return max;
+}
+
+
+
 function run() {
     const input = document.getElementById("input").value;
     const output = document.getElementById("output");
-    let out = "";
     let tempArr=[];
-    
+    let arr=[];
     let bol=true;
     try {
         JSON.parse(input);
@@ -14,7 +38,11 @@ function run() {
       if(Array.isArray(JSON.parse(input)))
           tempArr=JSON.parse(input);
 
-    let arr=[];
-    output.innerText = tempArr;
+    while(arr.length<tempArr.length){
+      let max=maxArray(tempArr);
+      arr.push(max);
+      tempArr[tempArr.indexOf(max)]=0;
+    }
+    output.innerText = arr;
 }
     
