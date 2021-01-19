6c2715eb275ef934357ceefa449757ec4baf75a5
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-01-19 13:53:01 +0200

    added the repo

--- q1.js
+++ q1.js
@@ -2,10 +2,7 @@ function run() {
     const input = document.getElementById("input").value;
     const output = document.getElementById("output");
     let out = "";
-/**/
-    // write your code here
-    // out = input + 5;
 
-/**/
+    
     output.innerText = out;
 }
