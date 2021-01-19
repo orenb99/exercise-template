7057f5c016e58852bf71e8fec602fa1d71ec0d83
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-01-19 19:07:27 +0200

    created q16

--- q1 copy 6.js
+++
@@ -1,8 +0,0 @@
-function run() {
-    const input = document.getElementById("input").value;
-    const output = document.getElementById("output");
-    let out = "";
-    
-    output.innerText = out;
-}
-    
---
+++ q16.js
@@ -0,0 +1,17 @@
+function run() {
+    const input = document.getElementById("input").value;
+    const output = document.getElementById("output");
+    let out = "";
+    let tempArr=[];
+    let bol=true;
+    try {
+        JSON.parse(input);
+    } catch {
+        bol=false;
+      }
+    if(bol===true)
+      if(Array.isArray(JSON.parse(input)))
+          tempArr=JSON.parse(input);
+    output.innerText = tempArr;
+}
+    
