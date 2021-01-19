7bd8de32db0b44a9631dc16a2fdb580e6ed50a94
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-01-19 19:22:00 +0200

    q16 done

--- q16.js
+++ q16.js
@@ -1,17 +1,28 @@
 function run() {
     const input = document.getElementById("input").value;
+    const input2 = document.getElementById("input2").value;
     const output = document.getElementById("output");
-    let out = "";
-    let tempArr=[];
-    let bol=true;
-    try {
-        JSON.parse(input);
-    } catch {
-        bol=false;
-      }
-    if(bol===true)
-      if(Array.isArray(JSON.parse(input)))
-          tempArr=JSON.parse(input);
-    output.innerText = tempArr;
+    let out="";
+    let minYear=Math.min(input,input2);
+    let maxYear=Math.max(input,input2);
+    for(let i=minYear;i<=maxYear;i++){
+    let leap=false;
+        if (i%4!==0) 
+            leap=false;
+
+        else if (i%100!==0) 
+            leap=true;
+            
+        else if (i%400!==0) 
+            leap=false;
+
+        else 
+            leap=true;
+        if(leap===true)
+            out+=i+"\n";
+    }
+
+
+    output.innerText = out;
 }
     
