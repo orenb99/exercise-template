d137cde9ac6dc8100c1dd70967649c21c210e384
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-01-20 12:25:59 +0200

    started q27

--- q27.js
+++ q27.js
@@ -1,18 +1,8 @@
 function run() {
     const input = document.getElementById("input").value;
     const output = document.getElementById("output");
-    let arr=[];
-    let out = "";
-    let bol=true;
-    try {
-        JSON.parse(input);
-  } catch {
-        bol=false;
-      }
-    if(bol===true)
-      if(Array.isArray(JSON.parse(input)))
-          arr=JSON.parse(input);
     
+
     output.innerText = out;
 }
     
