326e420086ca0cbe152c4621409389043bb72a31
orenb99 <77238204+orenb99@users.noreply.github.com>
2021-01-20 12:37:16 +0200

    q27 done

--- q27.js
+++ q27.js
@@ -1,8 +1,23 @@
+var library = [ 
+  { author: 'Bill Gates', title: 'The Road Ahead', libraryID: 1254},
+  { author: 'Steve Jobs', title: 'Walter Isaacson', libraryID: 4264},
+  { author: 'Suzanne Collins', title: 'Mockingjay: The Final Book of The Hunger Games', libraryID: 3245}
+  ];
+
 function run() {
     const input = document.getElementById("input").value;
     const output = document.getElementById("output");
-    
-
-    output.innerText = out;
+    let exist=false;
+    let arr=[];
+    for(let i=0; i<library.length;i++){
+      if(library[i].hasOwnProperty(input)){
+        arr.push(library[i][input]);
+        exist=true;
+      }
+    }
+    if(exist===false)
+      output.innerText = "no such property as "+input;
+    else
+      output.innerText= arr;
 }
     
